1bd6dbd4823e3e7a12574794805861e925210be3
Cecilia <ceciliawzx@qq.com>
2023-09-04 02:37:06 +0800

    fix path bug

--- src/util/data.tsx
+++ src/util/data.tsx
@@ -7,15 +7,15 @@ export const menuItems: MenuItem[] = [
   },
   {
     text: 'PROJECTS',
-    pageLink: '/~zw4021//projects',
+    pageLink: '/~zw4021/projects',
   },
   {
     text: 'INTERESTS',
-    pageLink: '/~zw4021//interests',
+    pageLink: '/~zw4021/interests',
   },
   {
     text: 'CONTACT',
-    pageLink: '/~zw4021//contact',
+    pageLink: '/~zw4021/contact',
   },
 ];
 
@@ -33,7 +33,7 @@ export const projects: Project[] = [
     Our objectives was to strengthen these areas and add a virtual memory implementation. 
     We faced challenges like concurrency, memory leak and data structure design, 
     but also learned a lot about C language and the overall operating system.`,
-    imgLink: '/~zw4021//pintos.jpg',
+    imgLink: '/~zw4021/pintos.jpg',
   },
   {
     title: 'WACC',
@@ -49,7 +49,7 @@ export const projects: Project[] = [
     We faced challenges of learning and applying new language, 
     and registers/stack management, 
     but gained comprehensive understanding of compilers.`,
-    imgLink: '/~zw4021//wacc.jpg',
+    imgLink: '/~zw4021/wacc.jpg',
   },
   {
     title: 'DRP',
@@ -70,7 +70,7 @@ export const projects: Project[] = [
       based on users feedback. It was a relatively short project, 
       but we were able to learn the process of delivering a product from the very beginning, 
       and also learned about the differences between school knowledge and designing for the real-world. `,
-    imgLink: '/~zw4021//drp.png',
+    imgLink: '/~zw4021/drp.png',
   },
 ];
 
