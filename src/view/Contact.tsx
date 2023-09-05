781b3e0d8f61fcda320e57de53dfc408a0286aab
Cecilia <ceciliawzx@qq.com>
2023-09-06 01:00:09 +0800

    contact implementation

--- src/view/Contact.tsx
+++ src/view/Contact.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { SocialMedia } from '../util/types';
 import { socialMedias } from '../util/data';
+import '../css/contact.css';
 
 export const Contact = () => {
   return (
-    <div className='window-main'>
+    <div
+      className='window-main'
+      style={{
+        width: '75%',
+        height: '100vh',
+      }}
+    >
       <div
         style={{
-          display: 'flex',
-          height: '100vh',
           width: '100%',
-          padding: '0 2%',
+          display: 'flex',
+          padding: '0 3%',
           flexDirection: 'column',
           justifyContent: 'flex-start',
           rowGap: '20%',
         }}
       >
         <ConactTitle />
-        <div style={{ width: '100%' }}>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+          }}
+          data-aos='fade-up'
+        >
           <SocialMedias />
+          <MessageWindow />
         </div>
       </div>
     </div>
@@ -41,12 +56,13 @@ const SocialMedias = () => {
     <div
       id='socialmedias-container'
       style={{
-        height: '50%',
-        width: '50%',
+        width: '40%',
         display: 'flex',
         flexDirection: 'column',
-        rowGap: '40%',
-        marginLeft: '5%',
+        rowGap: '10%',
+        alignItems: 'start',
+        paddingLeft: '50px',
+        minWidth: '350px',
       }}
     >
       {socialMedias.map((socialMedia, i) => (
@@ -65,7 +81,6 @@ const SocialMediaRow = ({ socialMedia }: { socialMedia: SocialMedia }) => {
         alignContent: 'start',
         alignItems: 'center',
       }}
-      data-aos='fade-up'
     >
       <img
         src={socialMedia.imgLink}
@@ -73,5 +88,37 @@ const SocialMediaRow = ({ socialMedia }: { socialMedia: SocialMedia }) => {
       />
       <p>{socialMedia.data}</p>
     </div>
+  );
+};
+
+const MessageWindow = () => {
+  return (
+    <div style={{ width: '30%', paddingLeft: '50px' }}>
+      <MessageForm />
+    </div>
+  );
+};
+
+const MessageForm = () => {
+  return (
+    <form
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        rowGap: '10%',
+      }}
+    >
+      <input type='text' className='form-control' placeholder='Name' />
+      <input type='text' className='form-control' placeholder='Email' />
+      <input type='text' className='form-control' placeholder='Subject' />
+      <textarea
+        id='contact-message'
+        cols={30}
+        rows={7}
+        className='form-control'
+        placeholder='Message'
+      />
+    </form>
   );
 };
